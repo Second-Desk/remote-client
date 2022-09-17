f7b4501c4f074cec1be50f677dd8504fdaabc7c6
Matthew Incardona <matthewincardona@matthews-mini.wireless.rit.edu>
2022-09-17 04:07:02 -0400

    Added socket io

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+const express = require("express");
+const expressApp = express();
+const http = require("http");
+const server = http.createServer(expressApp);
+const { Socket } = require("socket.io");
+const io = new http.Server(server);
+const port = process.env.PORT || 3000;
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: "favicon.ico",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -15,4 +24,17 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+});
+
+expressApp.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected!");
+  socket.emit("welcome", { message: "Welcome!", id: socket.id });
+});
+
+server.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
