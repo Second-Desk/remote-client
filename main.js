0165ce1357b9d8260f0d4b1ab1f0a53402982f5c
Matthew Incardona <matthewincardona@matthews-mini.wireless.rit.edu>
2022-09-18 03:23:31 -0400

    Calls can be made more easily

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const { v4: uuidv4 } = require("uuid");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
 // const robot = require("robotjs");
-
-var socket = require("socket.io-client")("http://localhost:3000");
-var interval;
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -21,45 +17,8 @@ const createWindow = () => {
 
   win.removeMenu();
   win.loadFile("index.html");
-
-  // socket.on("mouse-move", function (data) {
-  //   var obj = JSON.parse(data);
-  //   var x = obj.x;
-  //   var y = obj.y;
-
-  //   robot.moveMouse(x, y);
-  // });
-
-  // socket.on("mouse-click", function (data) {
-  //   robot.mouseClick();
-  // });
-
-  // socket.on("type", function (data) {
-  //   var obj = JSON.parse(data);
-  //   var key = obj.key;
-
-  //   robot.keyTap(key);
-  // });
 };
 
 app.whenReady().then(() => {
   createWindow();
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-ipcMain.on("start-share", function (event, arg) {
-  let uuid = "Mac Mini 440";
-  socket.emit("join-message", uuid);
-  event.reply("uuid", uuid);
 });
