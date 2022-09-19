a5634850745cf8cdf0f7bff554dc901ed3c7704c
Matthew Incardona <matthewincardona@matthews-mini.wireless.rit.edu>
2022-09-19 03:55:13 -0400

    Added keyboard and mouse support

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const { Peer } = require("peerjs");
-const stream2 = require("./preload.js");
+const { mouse, Point, keyboard, Key } = require("@nut-tree/nut-js");
 
 var peerId = "Mac-Mini-440";
 var peer = new Peer(peerId, {
@@ -30,9 +30,37 @@ peer.on("open", function () {
 // Receive messages
 peer.on("connection", function (conn) {
   conn.on("data", function (data) {
-    console.log(data);
+    // console.log(data);
+    remoteController(data);
   });
 });
+
+// Data handler for remote actions
+const remoteController = (data) => {
+  if (data.name != null) {
+    switch (data.name) {
+      case "mousePosition":
+        async () => {
+          const target = new Point(data.mouseX, data.mouseY);
+          await mouse.setPosition(target);
+        };
+        break;
+      case "mouseAction":
+        async () => {
+          await mouse.click(Button.LEFT);
+        };
+        break;
+      case "keyboardAction":
+        async () => {
+          console.log(data.key);
+          await keyboard.pressKey(Key.data.key);
+          await keyboard.releaseKey(Key.data.key);
+        };
+        break;
+      default:
+    }
+  }
+};
 
 //  Local client
 peer.on("call", function (call) {
@@ -43,7 +71,7 @@ peer.on("call", function (call) {
     .then(function (stream) {
       document.getElementById("connectionContainer").style.display = "none";
       remoteVideoContainer.style.display = "block";
-      var videoStream = remoteVideo.captureStream(25);
+      var videoStream = remoteVideo.captureStream(30);
       console.log("My stream: " + videoStream);
       call.answer(videoStream); // Answer the call with an A/V stream.
       call.on("stream", function (remoteStream) {
@@ -55,22 +83,3 @@ peer.on("call", function (call) {
     });
   peerCall = call; // export to a global variable
 });
-
-// peer.on("call", function (call) {
-//   console.log("Call successful!");
-//   // Answer the call, providing our mediaStream
-//   navigator.mediaDevices
-//     .getUserMedia({ video: true, audio: true })
-//     .then(function (stream) {
-//       document.getElementById("connectionContainer").style.display = "none";
-//       remoteVideoContainer.style.display = "block";
-//       call.answer(stream); // Answer the call with an A/V stream.
-//       call.on("stream", function (remoteStream) {
-//         remoteVideo.srcObject = remoteStream;
-//       });
-//     })
-//     .catch(function (err) {
-//       console.log("ERROR: " + err);
-//     });
-//   peerCall = call; // export to a global variable
-// });
