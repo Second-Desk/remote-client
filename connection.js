ab8b1051e4325c717f13a0956278e36de8ca7e81
Matthew Incardona <matthewincardona@pop-os.localdomain>
2022-10-22 02:39:09 -0400

    Changed peerjs server

--- connection.js
+++ connection.js
@@ -7,7 +7,7 @@ keyboard.config.autoDelayMs = 0;
 
 var peerId = "SD-RD-440";
 var peer = new Peer(peerId, {
-  host: "peerjs-env.eba-xwhvfsep.us-east-2.elasticbeanstalk.com/",
+  host: "peerjs-env.eba-xwhvfsep.us-east-2.elasticbeanstalk.com",
   secure: true,
   port: 443,
   debug: 2,
